0b8226d89a327a647e45e37a61c46ce419afaabc
unoexperto <unoexperto@users.noreply.github.com>
2024-01-18 17:59:00 +0000

    Clean up of the build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,15 @@
 import org.gradle.api.publish.maven.MavenPom
-import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.util.Date
-import java.io.FileInputStream
-import java.util.Properties
 import java.util.concurrent.atomic.AtomicReference
 import java.util.Scanner
 
 object Versions {
     val java = JavaVersion.VERSION_17 // can't go higher than 17 because of shadowJar
 
-    const val kotlin = "1.9.0"
+    const val kotlin = "1.9.22"
 
-    const val nettyBuffer = "4.1.95.Final" // https://mvnrepository.com/artifact/io.netty/netty-buffer
-    const val reactor = "3.5.8" // https://mvnrepository.com/artifact/io.projectreactor/reactor-core
-    const val reactorNetty = "1.1.9" // https://mvnrepository.com/artifact/io.projectreactor.netty/reactor-netty
+    const val reactorNetty = "1.1.15" // https://mvnrepository.com/artifact/io.projectreactor.netty/reactor-netty
     const val moshi = "1.15.0" // https://mvnrepository.com/artifact/com.squareup.moshi/moshi
-    const val moshix = "0.24.0" // https://mvnrepository.com/artifact/dev.zacsweers.moshix/dev.zacsweers.moshix.gradle.plugin
-    const val classgraph = "4.8.161" // https://mvnrepository.com/artifact/io.github.classgraph/classgraph
-
-    const val log4j = "2.20.0" // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
-
-    const val jdbi3 = "3.39.1" // https://mvnrepository.com/artifact/org.jdbi/jdbi3-core
-    const val postgresql = "42.6.0" // https://mvnrepository.com/artifact/org.postgresql/postgresql
-    const val jackson = "2.14.2" // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core
-    const val h2 = "2.1.214" // https://mvnrepository.com/artifact/com.h2database/h2
-    const val walkmindSerializers = "1.7"
-    const val hikariCP = "5.0.1" // https://mvnrepository.com/artifact/com.zaxxer/HikariCP
-    const val reactorKotlinExt = "1.2.2" // https://mvnrepository.com/artifact/io.projectreactor.kotlin/reactor-kotlin-extensions
-    const val commonsCsv = "1.10.0" // https://mvnrepository.com/artifact/org.apache.commons/commons-csv/
-    const val telegramBots = "6.5.0"
 
     val commitHash by lazy {
         val scanner = Scanner(Runtime.getRuntime().exec("git rev-parse HEAD").inputStream)
@@ -41,16 +21,10 @@ object Versions {
 }
 
 plugins {
-    kotlin("jvm") version "1.9.0"
-    id("dev.zacsweers.moshix") version "0.24.0"
+    kotlin("jvm") version "1.9.22"
     idea
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("maven-publish")
-//    `java-library`
-}
-
-kotlin {
-    //    experimental.coroutines = Coroutines.ENABLE
 }
 
 project.group = "com.walkmind.extensions"
@@ -125,17 +99,11 @@ publishing {
         maven {
 //            name = "GitHubPackages"
             url = uri("s3://${awsCreds["maven_bucket"]!!}/")
-//            url = uri("https://maven.pkg.github.com/unoexperto/maven/")
             credentials(AwsCredentials::class) {
 
                 accessKey = awsCreds["aws_access_key_id"]
                 secretKey = awsCreds["aws_secret_access_key"]
-//                sessionToken = "someSTSToken" // optional
             }
-//            credentials {
-//                username = "unoexperto"
-//                password = "xxxx"
-//            }
         }
     }
 
@@ -211,32 +179,11 @@ configurations {
     }
 }
 
-//sourceSets {
-//    main {
-//        java.srcDir("src/core/java")
-//    }
-//}
-
-//java {
-//    sourceCompatibility = JavaVersion.VERSION_1_8
-//    targetCompatibility = JavaVersion.VERSION_1_8
-//}
-
 tasks {
 
     withType<JavaCompile> {
         sourceCompatibility  = Versions.java.majorVersion
         targetCompatibility = Versions.java.majorVersion
-
-//        options.compilerArgs.add("--enable-preview")
-//        inputs.property("moduleName", moduleName)
-//        doFirst {
-//            options.compilerArgs = listOf(
-//                "--module-path", classpath.asPath,
-//                "--patch-module", "$moduleName=${sourceSets["main"].output.asPath}"
-//            )
-//            classpath = files()
-//        }
     }
 
     withType<KotlinCompile> {
@@ -261,23 +208,14 @@ tasks {
     }
 
     withType<Test>().all {
-//        jvmArgs = listOf("--enable-preview")
         testLogging.showStandardStreams = true
         testLogging.showExceptions = true
         useJUnitPlatform {
         }
     }
 
-    withType<JavaExec>().all {
-//        jvmArgs = listOf("--enable-preview")
-    }
-
     withType<Wrapper>().all {
         gradleVersion = "8.0"
         distributionType = Wrapper.DistributionType.BIN
-    }
-
-    withType<JavaCompile>().all {
-//        options.compilerArgs.addAll(listOf("--enable-preview", "-Xlint:preview"))
     }
 }
